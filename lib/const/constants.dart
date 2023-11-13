f1fa8b3cbb91b263b46f5e9d6ae7cfc381bb27bc
habeebputhiyakath <irfanputhiyakat@gmail.com>
2023-11-13 23:49:46 +0530

    colors changing

--- lib/const/constants.dart
+++ lib/const/constants.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
-const kTextColor = Color(0xFF535353);
-const kTextLightColor = Color(0xFFACACAC);
+const kTextColor = Color.fromARGB(255, 68, 66, 66);
+const kTextLightColor = Color.fromARGB(255, 176, 169, 169);
 const kDefaultPadding = 20.0;
 const kdescription =
-    "The durable rubber outsole offers excellent traction on various surfaces. From city streets to nature trails, these shoes provide the grip you need for a confident and stable stride";
+    "The durable rubber outsole offers excellent traction on various surfaces. From city streets to nature trails, shoes provide the grip you need for a confident and stable stride";
